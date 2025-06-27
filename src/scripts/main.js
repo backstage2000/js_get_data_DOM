b722d33688336b44c61f808baf643d489cfa383e
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-06-27 12:33:15 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const populationTotla = [];
+
+for (const ch of population) {
+  const num = Number(ch.textContent.replace(/,/g, ''));
+
+  populationTotla.push(num);
+}
+
+const result = populationTotla.reduce((acc, value) => acc + value);
+
+const totalSum = result / populationTotla.length;
+
+const totalPopulation = document.querySelector('.total-population');
+
+const averagePopulation = document.querySelector('.average-population');
+
+const totalFormatted = totalSum.toLocaleString('en-US');
+const averageFormatted = result.toLocaleString('en-US');
+
+averagePopulation.textContent = totalFormatted;
+totalPopulation.textContent = averageFormatted;
